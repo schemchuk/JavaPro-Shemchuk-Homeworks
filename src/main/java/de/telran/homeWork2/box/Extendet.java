f0d9ec779e12620441d28033ff6d69e905f8c0e0
shemchuk <shemchukaga@gmail.com>
2023-11-13 18:15:59 +0100

    Homework2 punkt 1, 2, 3, 4

---
+++ src/main/java/de/telran/homeWork2/box/Extendet.java
@@ -0,0 +1,11 @@
+package de.telran.homeWork2.box;
+
+public class Extendet {
+    public static void main(String[] args) {
+
+        MyBox myBox = new MyBox(23,13, 6, 23456);
+
+        System.out.println("Обем коробки " + myBox.volumeBox() + " единиц кубических");
+        System.out.println( "Потребуется " + myBox.hovBoxesNeed() + " коробок");
+    }
+}
