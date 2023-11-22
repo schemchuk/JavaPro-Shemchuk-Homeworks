ed587937f2d70f1a94a6aec05c516157fc2a2094
shemchuk <shemchukaga@gmail.com>
2023-11-22 21:36:48 +0100

    Homework5

---
+++ src/main/java/de/telran/module1/homeWork5/monthInYear/MonthsInYear.java
@@ -0,0 +1,32 @@
+package de.telran.module1.homeWork5.monthInYear;
+
+public enum MonthsInYear {
+    JANUARY("Январь"),
+    FEBRUARY("Февраль"),
+    MARCH("Март"),
+    APRIL("Апрель"),
+    MAY("Май"),
+    JUNE("Июнь"),
+    JULY("Июль"),
+    AUGUST("Август"),
+    SEPTEMBER("Сентябрь"),
+    OCTOBER("Октябрь"),
+    NOVEMBER("Ноябрь"),
+    DECEMBER("Декабрь"),
+    NEW_NASA_MONTH("Пупень");
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    MonthsInYear(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "MonthsInYear{}";
+    }
+}
---
+++ src/main/java/de/telran/module1/homeWork5/monthInYear/SimpleEnum.java
@@ -0,0 +1,35 @@
+package de.telran.module1.homeWork5.monthInYear;
+
+public class SimpleEnum {
+    public static void main(String[] args) {
+        MonthsInYear month1 = MonthsInYear.JANUARY;
+        MonthsInYear month2 = MonthsInYear.FEBRUARY;
+        MonthsInYear month3 = MonthsInYear.MARCH;
+        MonthsInYear month4 = MonthsInYear.APRIL;
+        MonthsInYear month5 = MonthsInYear.MAY;
+        MonthsInYear month6 = MonthsInYear.JULY;
+        MonthsInYear month7 = MonthsInYear.JUNE;
+        MonthsInYear month8 = MonthsInYear.AUGUST;
+        MonthsInYear month9 = MonthsInYear.SEPTEMBER;
+        MonthsInYear month10 = MonthsInYear.OCTOBER;
+        MonthsInYear month11 = MonthsInYear.NOVEMBER;
+        MonthsInYear month12 = MonthsInYear.DECEMBER;
+        MonthsInYear month13 = MonthsInYear.NEW_NASA_MONTH;
+
+
+        WhatWillWeDrink whatWillWeDrink = new WhatWillWeDrink(MonthsInYear.NEW_NASA_MONTH);
+
+
+        if(whatWillWeDrink.determineSeason().equals("Winter")) {
+            System.out.println("Drink Rom");
+        } else if (whatWillWeDrink.determineSeason().equals("Spring")) {
+            System.out.println("Drink wine");
+        } else if (whatWillWeDrink.determineSeason().equals("Summer")) {
+            System.out.println("Drink beer");
+        } else if (whatWillWeDrink.determineSeason().equals("Autumn")) {
+            System.out.println("Drink whiskey");
+        } else {
+            System.out.println("Это сомнительный месяц придуман недавно. Не пей алкогольных напитков");
+        }
+    }
+}
