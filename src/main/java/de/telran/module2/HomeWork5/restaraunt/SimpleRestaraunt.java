f94c3f9dc00c5105e970f9edab3a237848be426e
shemchuk <shemchukaga@gmail.com>
2023-12-06 18:39:03 +0100

    Honework5 LinkedHashSet, TreeSet

---
+++ src/main/java/de/telran/module2/HomeWork5/restaraunt/SimpleRestaraunt.java
@@ -0,0 +1,11 @@
+package de.telran.module2.HomeWork5.restaraunt;
+
+public class SimpleRestaraunt {
+    public static void main(String[] args) {
+        RestarauntWork restor = new RestarauntWork();
+        restor.addOrder(new Order("Огурцы соленые",1) );
+        restor.addOrder(new Order("Капуста квашенная",2));
+        restor.addOrder(new Order("Томаты моченные",3));
+        restor.addOrder(new Order("Щи! -с",4));
+    }
+}
