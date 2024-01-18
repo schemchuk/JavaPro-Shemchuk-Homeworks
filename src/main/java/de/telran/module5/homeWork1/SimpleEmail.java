0c611651bc5caa75c6d0b84f14eccbc66bb83ea0
shemchuk <shemchukaga@gmail.com>
2024-01-18 15:58:37 +0100

    my homeWork with regular expers

--- src/main/java/de/telran/module5/homeWork1/SimpleEmail.java
+++ src/main/java/de/telran/module5/homeWork1/SimpleEmail.java
@@ -5,11 +5,11 @@ import java.util.regex.Pattern;
 
 public class SimpleEmail {
     public static void main(String[] args) {
-        //  "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\\.[A-Za-z]{2,4}\\b"));
+        //  [A-Za-z0-9._%+-]+@[A-Za-z0-9]+\\.[A-Za-z]{2,4};
         // Проверяем правильность шаблона
-        System.out.println("myemail@gmail.com".matches("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\\.[A-Za-z]{2,4}\\b"));
+        System.out.println("myemail@gmail.com".matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\\.[A-Za-z]{2,4}"));
         // Находим индекс входа
-        Pattern p = Pattern.compile("\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\\.[A-Za-z]{2,4}\\b");
+        Pattern p = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9]+\\.[A-Za-z]{2,4}");
         Matcher m = p.matcher("My email is myemail@gmail.com");
 
         while (m.find()){
