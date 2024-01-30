430f5d2aa1b9ef046f28ee7a0f2dfc46f79349a3
shemchuk <shemchukaga@gmail.com>
2024-01-30 19:20:58 +0100

    my homeWorks add semafor

---
+++ src/main/java/de/telran/module6/homeWork3/shoot/Shooting.java
@@ -0,0 +1,28 @@
+package de.telran.module6.homeWork3.shoot;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Shooting extends Thread {
+    private static AtomicBoolean isHit = new AtomicBoolean(false);
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    public Shooting(String name) {
+        Random random = new Random();
+
+        while (!isHit.get()) {
+            if (random.nextBoolean()) {
+                isHit.set(true);
+                System.out.println("Мишень поражена и разрушена");
+            } else {
+                System.out.println("Промах. Мишень целая");
+            }
+        }
+
+
+    }
+}
