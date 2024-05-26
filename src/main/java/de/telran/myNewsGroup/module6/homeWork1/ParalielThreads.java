07a33ae04c4343df06d3cd68aef9f5c2ffb58fa4
shemchuk <shemchukaga@gmail.com>
2024-05-26 13:34:20 +0200

    my homeWork  threads finaly

--- src/main/java/de/telran/myNewsGroup/module6/homeWork1/ParalielThreads.java
+++ src/main/java/de/telran/myNewsGroup/module6/homeWork1/ParalielThreads.java
@@ -1,15 +1,23 @@
 package de.telran.myNewsGroup.module6.homeWork1;
 
 public class ParalielThreads {
+    /**
+     * : посчитать количество целых чисел в диапазоне от Integer.MINVALUE до Integer.MAXVALUE,
+     * которые делятся на заданное целое число без остатка.
+     * Решить данную задачу последовательно и параллельно в нескольких потоках. Сравнить время выполнения.
+     */
 
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
+        System.out.println("Выполняем задачу с распаралеленными потоками");
 
         int number = 5;
-        Task task1 = new Task(Integer.MIN_VALUE, -500_000_000, number);
-        Task task2 = new Task(-500_000_000, 1_000_000_000,number);
-        Task task3 = new Task(1_000_000_000,2_000_000_000, number);
-        Task task4 = new Task(2_000_000_000 , Integer.MAX_VALUE, number);
+
+        long startParallel = System.currentTimeMillis();
+
+        ParalielThreads.Task task1 = new ParalielThreads.Task(Integer.MIN_VALUE, -500_000_000, number);
+        ParalielThreads.Task task2 = new ParalielThreads.Task(-500_000_000, 1_000_000_000, number);
+        ParalielThreads.Task task3 = new ParalielThreads.Task(1_000_000_000, 2_000_000_000, number);
+        ParalielThreads.Task task4 = new ParalielThreads.Task(2_000_000_000 , Integer.MAX_VALUE, number);
 
         Thread thread1 = new Thread(task1);
         Thread thread2 = new Thread(task2);
@@ -34,19 +42,42 @@ public class ParalielThreads {
 
         int totalCount = task1.count + task2.count + task3.count + task4.count;
 
-        long end = System.currentTimeMillis();
+
+        long endParallel = System.currentTimeMillis();
 
         System.out.println("Total numbner of numbers divisible by " + number + " is " + totalCount );
 
-        System.out.println("Elapsed time: " + (end - start));
+        System.out.println("Elapsed time for parallel: " + (endParallel - startParallel));
 
+        System.out.println("Задача выполнена");
+
+        System.out.println("Запускаем выполнение задачи в однопоточном режиме");
+
+        long startConsistentSolution = System.currentTimeMillis();
+        int totalCountWithoutTreads = countWithoutTreads(number);
+        long endConsistentSolution = System.currentTimeMillis();
+
+        System.out.println("Total number of numbers divisible by " + number + " is " + totalCountWithoutTreads);
+        System.out.println("Elapsed time for consistent solution: " + (endConsistentSolution - startConsistentSolution));
+
+        System.out.println(" Завершение задачи");
+    }
+
+    public static  int countWithoutTreads(int number) {
+        int count = 0;
+        for (int i = Integer.MIN_VALUE; i <= Integer.MAX_VALUE; i++) {
+            if( i % number == 0) {
+                count++;
+            }
+        }
+        return count;
     }
 
     public static class Task implements Runnable {
         int start;
         int end;
         int number;
-        int count;
+        public int count;
 
         public Task(int start, int end, int number) {
             this.start = start;
